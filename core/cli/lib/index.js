2b36a053798941848bf9c531801bbf02b6e41e9c
diandiandidi519 <978267807@qq.com>
2021-11-22 08:47:54 +0800

    ✨ feat(core/cli): 检查node版本

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,14 +1,31 @@
 module.exports = core;
 
+const semver = require("semver");
+const colors = require("colors/safe");
+
 const pkg = require("../package.json");
 const log = require("@diandiandidi-cli/log");
+const constant = require("./const");
 
 function checkPkgVersion() {
-  console.log(pkg.version);
+  log.notice("cli", pkg.version);
+}
+
+function checkNodeVersion() {
+  // 获取当前node版本号
+  const currentVersion = process.version;
+  // 比对最低版本号
+  const lowestVersion = constant.LOWEST_NODE_VERSION;
+  if (semver.gt(lowestVersion, currentVersion)) {
+    throw new Error(colors.red(`需要安装 v${lowestVersion}已上的版本 Node.js`));
+  }
 }
 
 function core() {
-  console.log("exec core");
-  checkPkgVersion();
-  log.notice("cli", pkg.version);
+  try {
+    checkPkgVersion();
+    checkNodeVersion();
+  } catch (e) {
+    log.error(e.message);
+  }
 }
